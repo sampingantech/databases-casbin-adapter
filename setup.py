79c6e66db163012c5073a73c7da87579d99b48f1
setiawanisasi <azaqokay@gmail.com>
2021-04-15 02:23:18 +0700

    add setup file

---
+++ setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+with open(f"{HERE}/requirements.txt") as file:
+    requirements = file.read().splitlines()
+
+setup(
+    name="databases-casbin-adapter",
+    version="1.0.0",
+    description="This is an Adapter for PyCasbin that implemented using Databases connection to achieve async process",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sampingantech/databases-casbin-adapter",
+    author="Isa Setiawan Abdurrazaq",
+    author_email="isasetiawan@protonmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+)
